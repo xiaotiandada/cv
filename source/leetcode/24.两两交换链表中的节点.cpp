86d065753b30f4f1da71b3caa79f998b978cbd20
xiaotian <istianlei@qq.com>
2022-12-28 00:21:36 +0800

    swapPairs

---
+++ source/leetcode/24.两两交换链表中的节点.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=cpp
+ *
+ * [24] 两两交换链表中的节点
+ * 方法一：递归
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode* newHead = head->next;
+        head->next = swapPairs(newHead->next);
+        newHead->next = head;
+        return newHead;
+    }
+};
+// @lc code=end
+
