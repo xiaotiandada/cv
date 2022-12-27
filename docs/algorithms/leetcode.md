8ebf62315872131287f57c31a79db6d8bd53cfb1
xiaotian <istianlei@qq.com>
2022-12-27 23:09:48 +0800

    feat: 3sum

--- docs/algorithms/leetcode.md
+++ docs/algorithms/leetcode.md
@@ -168,6 +168,7 @@ public:
 
 - https://leetcode.cn/problems/3sum/
 - https://leetcode.com/problems/3sum/solutions/1363302/c-brute-force-optimal-two-pointer-method-commented-time-o-n-2-auxiliary-space-o-1/?orderBy=most_votes&languageTags=cpp
+- https://leetcode.cn/problems/3sum/solution/san-shu-zhi-he-by-leetcode-solution/
 
 **暴力解法**
 
@@ -197,4 +198,50 @@ public:
         return ans;
     }
 };
+```
+
+**排序 + 双指针**
+
+```cpp
+vector<vector<int>> threeSum(vector<int>& nums) {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+
+    // 枚举 a
+    for (int first = 0; first < n; ++first) {
+        // 需要和上一次枚举的数不相同
+        if (first > 0 && nums[first] == nums[first - 1]) {
+            continue;
+        }
+
+        // c 对应的指针初始指向数组的最右端
+        int third = n - 1;
+        int target = -nums[first];
+
+        // 枚举 b
+        for (int second = first + 1; second < n; ++second) {
+            if (second > first + 1 && nums[second] == nums[second - 1]) {
+                continue;
+            }
+
+            // 需要保证 b 的指针在 c 的指针的左侧
+            while (second < third && nums[second] + nums[third] > target) {
+                --third;
+            }
+
+            // 如果指针重合，随着 b 后续的增加
+            // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+            if (second == third) {
+                break;
+            }
+
+            if (nums[second] + nums[third] == target) {
+                ans.push_back({nums[first], nums[second], nums[third]});
+            }
+        }
+    }
+
+    return ans;
+}
 ```
--- source/leetcode/3sum/main.cpp
+++ source/leetcode/3sum/main.cpp
@@ -32,6 +32,48 @@ public:
         vector<vector<int>> ans(s.begin(), s.end());
         return ans;
     }
+
+    vector<vector<int>> threeSum1(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        // 枚举 a
+        for (int first = 0; first < n; ++first) {
+            // 需要和上一次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = -nums[first];
+
+            // 枚举 b
+            for (int second = first + 1; second < n; ++second) {
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+
+                if (nums[second] + nums[third] == target) {
+                    ans.push_back({nums[first], nums[second], nums[third]});
+                }
+            }
+        }
+
+        return ans;
+    }
 };
 
 int main()
@@ -41,7 +83,10 @@ int main()
     // 2,7,11,15
     vector<int> test = { -1,0,1,2,-1,-4 };
 
-    vector<vector<int>> result = SolutionFn.threeSum(test);
+    vector<vector<int>> result = SolutionFn.threeSum1(test);
 
-    cout << "result" << " " << result[0][0] << result[0][1] << result[0][2] << endl;
+    for (int i = 0; i < result.size(); i++) {
+        cout << "result" << "[" << result[i][0] << "," << result[i][1]  << ',' << result[i][2] << "]" << endl;
+    }
+
 }
