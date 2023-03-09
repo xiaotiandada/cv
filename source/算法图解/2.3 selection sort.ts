62bd31d8dd77b9a4263efaab3ede2b948a9db9cb
xiaotian <istianlei@qq.com>
2023-03-09 13:55:04 +0800

    feat: selection sort

---
+++ source/算法图解/2.3 selection sort.ts
@@ -0,0 +1,19 @@
+function selectionSort(arr: number[]): number[] {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([5, 3, 6, 2, 10]));
