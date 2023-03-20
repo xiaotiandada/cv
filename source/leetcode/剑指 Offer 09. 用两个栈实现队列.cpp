75c4ec8415683cca40f1a5dd09c47b12cd6646e6
xiaotian <istianlei@qq.com>
2023-03-21 01:56:58 +0800

    Create 剑指 Offer 09. 用两个栈实现队列.cpp

---
+++ source/leetcode/剑指 Offer 09. 用两个栈实现队列.cpp
@@ -0,0 +1,40 @@
+class CQueue {
+    stack<int> inStack, outStack;
+
+private:
+    void in2out() {
+        while (!inStack.empty()) {
+            outStack.push(inStack.top());
+            inStack.pop();
+        }
+    }
+
+public:
+    CQueue() {
+
+    }
+    
+    void appendTail(int value) {
+        inStack.push(value);
+    }
+    
+    int deleteHead() {
+        if (outStack.empty()) {
+            if (inStack.empty()) {
+                return -1;
+            }
+            in2out();
+        }
+
+        int value = outStack.top();
+        outStack.pop();
+        return value;
+    }
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue* obj = new CQueue();
+ * obj->appendTail(value);
+ * int param_2 = obj->deleteHead();
+ */
